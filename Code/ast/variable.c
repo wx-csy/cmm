c4977c4f6ae1c9e80d9f6120739325beae864cc1
wx-csy <sy_chen@smail.nju.edu.cn>
2019-05-07 14:37:49 +0800

    add array param

--- Code/ast/variable.c
+++ Code/ast/variable.c
@@ -28,9 +28,19 @@ void Variable_Add_Initializer(Variable *var, Expression *init) {
 void Variable_IR_Generate_Declaration(Variable *var, bool is_param) {
     var->ir_id = ir_newvar();
     if (is_param) {
-        // only size=4 parameter supported
-        assert(var->valtype->width == 4);
-        ir_gen_add_with_comment(ir_make_param(ir_make_var(var->ir_id)), "param '%s'", var->name);
+        if (Type_Is_Basic(var->valtype)) {
+            ir_gen_add_with_comment(ir_make_param(ir_make_var(var->ir_id)), "param '%s'", var->name);
+        } else {
+            ir_val tmpparam = ir_make_var(ir_newvar()), param = ir_make_var(var->ir_id), pptr = ir_make_var(ir_newvar());
+            ir_gen_add_with_comment(ir_make_param(tmpparam), "param '%s' (ptr)", var->name);
+            ir_gen_add_with_comment(ir_make_dec(param, var->valtype->width), "param '%s'", var->name);
+            ir_gen_add(ir_make_assign(pptr, ir_make_ref(param)));
+            for (int i = 0; i < var->valtype->width; i += 4) {
+                ir_gen_add(ir_make_assign(ir_make_deref(pptr.varid), ir_make_deref(tmpparam.varid)));
+                ir_gen_add(ir_make_binary_op(IRBOP_ADD, pptr, pptr, ir_make_immd(4)));
+                ir_gen_add(ir_make_binary_op(IRBOP_ADD, tmpparam, tmpparam, ir_make_immd(4)));
+            }
+        }
     } else {
         ir_gen_add_with_comment(ir_make_dec(ir_make_var(var->ir_id), var->valtype->width), "var '%s'", var->name);
         if (var->initializer)
